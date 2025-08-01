d5cb745a1589bef9e6257ce4591693eadd253e21
Greg Bowler <greg@g105b.com>
2025-08-01 12:19:30 +0100

    test: LinkNormaliser fixes external docs links

--- class/Content/LinkNormaliser.php
+++ class/Content/LinkNormaliser.php
@@ -5,9 +5,33 @@ use Gt\Dom\Element;
 use Gt\Http\Uri;
 
 class LinkNormaliser {
-	public function __construct(Element $article) {}
+	const string CLASS_EXTERNAL_LINK = "link-external";
+	const CLASS_DOCS_LINK = "link-docs";
+
+	public function __construct(
+		private readonly Element $article
+	) {}
 
 	public function normalise(Uri $currentUri):void {
+		$linkList = $this->article->querySelectorAll("a[href]");
+		if($linkList->length === 0) {
+			return;
+		}
 
+		$host = $currentUri->getHost();
+		foreach($linkList as $link) {
+			$linkUri = new Uri($link->href);
+			$linkHost = $linkUri->getHost();
+			$linkPath = $linkUri->getPath();
+			if($linkHost !== $host) {
+				if($linkHost === "www.php.gt" && str_starts_with($linkPath, "/docs/")) {
+					$link->classList->add(self::CLASS_DOCS_LINK);
+					$link->href = $linkPath;
+				}
+				else {
+					$link->classList->add(self::CLASS_EXTERNAL_LINK);
+				}
+			}
+		}
 	}
 }
--- test/phpunit/Content/LinkNormaliserTest.php
+++ test/phpunit/Content/LinkNormaliserTest.php
@@ -20,4 +20,49 @@ class LinkNormaliserTest extends TestCase {
 		$htmlAfter = $element->innerHTML;
 		self::assertSame($htmlBefore, $htmlAfter);
 	}
+
+	public function testNormalise_hasExternalLink():void {
+		$document = new HTMLDocument("<article><h1>Hello, <a href='https://www.phpunit.de'>PHPUnit</a>!</h1></article>");
+		$element = $document->querySelector("article");
+		$uri = self::createMock(Uri::class);
+		$uri->method("getHost")->willReturn("localhost");
+
+		$htmlBefore = $element->innerHTML;
+		$sut = new LinkNormaliser($element);
+		$sut->normalise($uri);
+
+		$htmlAfter = $element->innerHTML;
+		self::assertNotSame($htmlBefore, $htmlAfter);
+		$link = $document->querySelector("article a");
+		self::assertTrue($link->classList->contains(LinkNormaliser::CLASS_EXTERNAL_LINK));
+	}
+
+	public function testNormalise_hasDocsLink():void {
+		$document = new HTMLDocument("<article><h1>Hello, <a href='https://www.php.gt/docs/webengine/getting-started/'>PHP.GT</a>!</h1></article>");
+		$element = $document->querySelector("article");
+		$uri = self::createMock(Uri::class);
+		$uri->method("getHost")->willReturn("localhost");
+
+		$htmlBefore = $element->innerHTML;
+		$sut = new LinkNormaliser($element);
+		$sut->normalise($uri);
+
+		$htmlAfter = $element->innerHTML;
+		self::assertNotSame($htmlBefore, $htmlAfter);
+		$link = $document->querySelector("article a");
+		self::assertTrue($link->classList->contains(LinkNormaliser::CLASS_DOCS_LINK));
+	}
+
+	public function testNormalise_docsLinkAbsolute():void {
+		$document = new HTMLDocument("<article><h1>Hello, <a href='https://www.php.gt/docs/webengine/getting-started/'>PHP.GT</a>!</h1></article>");
+		$element = $document->querySelector("article");
+		$uri = self::createMock(Uri::class);
+		$uri->method("getHost")->willReturn("localhost");
+
+		$sut = new LinkNormaliser($element);
+		$sut->normalise($uri);
+
+		$link = $document->querySelector("article a");
+		self::assertSame("/docs/webengine/getting-started/", $link->href);
+	}
 }
