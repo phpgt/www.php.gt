9bbe27655a99efb48b590bff76ec3be4b1f7e2c0
Greg Bowler <greg@g105b.com>
2025-08-01 18:29:59 +0100

    feature: redirect to proper cases

---
+++ page/docs/@repo/@doc.php
@@ -0,0 +1,26 @@
+<?php
+use Gt\Dom\HTMLDocument;
+use Gt\Http\Response;
+use Gt\Routing\Path\DynamicPath;
+
+function go(
+	DynamicPath $dynamicPath,
+	HTMLDocument $document,
+	Response $response,
+):void {
+	$repoPage = $dynamicPath->get("repo");
+	$docPage = $dynamicPath->get("doc");
+
+// Ensure the URL is cased correctly according to the repo names.
+	foreach(glob("data/content/*/") as $dir) {
+		$dirName = pathinfo($dir, PATHINFO_FILENAME);
+		if(strtolower($dirName) === $repoPage) {
+			$response->redirect("/docs/$dirName/$docPage");
+		}
+	}
+
+	if(strtolower($docPage) === "home") {
+		$firstHeading = $document->querySelector("article h1");
+		$firstHeading->innerText = "$repoPage documentation";
+	}
+}
