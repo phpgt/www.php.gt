10ca4e7170eb2ca0f8d1658e24a5958661a5c32c
Greg Bowler <greg@g105b.com>
2026-07-12 18:49:51 +0100

    feature: contributing page

---
+++ page/contributing.php
@@ -0,0 +1,13 @@
+<?php
+use GT\DomTemplate\Binder;
+use GT\Website\Content\MarkdownFile;
+use GT\Website\Content\MarkdownPage;
+
+function go(Binder $binder):void {
+//	$contributingMarkdownFile = "data/content/WebEngine/CONTRIBUTING.md";
+//	if(!file_exists($contributingMarkdownFile)) {
+//		return;
+//	}
+
+	$markdownPage = new MarkdownPage("data/content");
+}
